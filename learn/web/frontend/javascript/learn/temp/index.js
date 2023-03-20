7b155eb18760cbaa1852ff18dbce92e17797fd41
Hemkant <hemkantkushwaha831963@gmail.com>
2023-03-20 10:02:14 +0530

    first commit

---
+++ learn/web/frontend/javascript/learn/temp/index.js
@@ -0,0 +1,11 @@
+// Hoisting 
+
+// x = 34;
+let hello  = () => {
+    "use strict";
+    let x = {fname:["hemkant", "patel"], lname:"kushwaha", salary:280000, job:"Fullstack Engineer" }
+      
+    const elem = document.getElementById("demo"); // Find an element
+    elem.innerHTML = Date(); // Display x in the element
+   
+}
